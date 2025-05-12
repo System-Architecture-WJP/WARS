d02079cb6c89cc1346ad432bc7f5bf159796273a
Mirian Shilakadze <mirian.shilakadze@gmail.com>
2025-05-12 18:54:10 +0400

    Introduce Assembler class

--- src/main/java/com/wars/App.java
+++ src/main/java/com/wars/App.java
@@ -2,30 +2,13 @@ package com.wars;
 
 import com.wars.instruction.Instruction;
 import com.wars.instruction.InstructionRegistry;
+import com.wars.assembler.Assembler;
 
 import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        String code = """
-                # example
-                sw 0 1 0
-                addi 2 1 4
-                addi 4 3 -1
-                j 20
-                """;
-
-        Arrays.stream(code.split("\\n")).forEach(App::extract);
-    }
-
-    private static void extract(String line) {
-        if (line.isBlank() || line.startsWith("#")) return;
-
-        String[] split = line.split("\\s+");
-        String mnemonic = split[0];
-        String[] operands = Arrays.copyOfRange(split, 1, split.length);
-
-        Instruction instruction = InstructionRegistry.create(mnemonic, operands);
-        System.out.println(instruction.toBinaryString());
+        Assembler asm = new Assembler(System.in, System.out);
+        asm.assembleToBinaryString();
     }
 }

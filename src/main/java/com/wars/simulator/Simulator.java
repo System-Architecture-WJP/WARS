2725f1cdfa3399b45fbf8d2d6f056a3737f48946
Konstantine Machavariani <kotiko2004@gmail.com>
2025-06-29 14:55:02 +0400

    add execution of instruction logic by PC
    Signed-off-by: Konstantine Machavariani <kotiko2004@gmail.com>

--- src/main/java/com/wars/simulator/Simulator.java
+++ src/main/java/com/wars/simulator/Simulator.java
@@ -2,55 +2,67 @@ package com.wars.simulator;
 
 import com.wars.instruction.InstructionRegistry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Simulator {
-    public static Configuration simulate(int[] ints) {
+
+    public static Configuration simulate(int[] instructions) {
         var configuration = new Configuration();
 
-        for (int bin : ints) {
-            simulateStep(configuration, bin);
+        // Mimic having the program in memory.
+        Map<Long, Integer> programMemory = new HashMap<>();
+        for (int i = 0; i < instructions.length; i++) {
+            programMemory.put((long) i * 4, instructions[i]);
         }
+
+        while (programMemory.containsKey(configuration.getPC())) {
+            int instructionWord = programMemory.get(configuration.getPC());
+            simulateStep(configuration, instructionWord);
+        }
+
         return configuration;
     }
 
-    private static void simulateStep(Configuration configuration, int bin) {
-        int opcode = (bin >> 26);
+    private static void simulateStep(Configuration configuration, int instructionWord) {
+        int opcode = (instructionWord >> 26);
 
         if (opcode == 0b000000 || opcode == 0b010000) {
-            simulateRType(configuration, bin);
+            simulateRType(configuration, instructionWord);
         } else if (opcode == 0b000010 || opcode == 0b000011) {
-            simulateJType(configuration, bin);
+            simulateJType(configuration, instructionWord);
         } else {
-            simulateIType(configuration, bin);
+            simulateIType(configuration, instructionWord);
         }
     }
 
-    private static void simulateIType(Configuration configuration, int bin) {
-        int opcode = (bin >> 26);
-        int rs = (bin >>> 21);
-        int rt = (bin >>> 16);
-        int imm = bin & 0xFFFF;
+    private static void simulateIType(Configuration configuration, int instructionWord) {
+        int opcode = (instructionWord >> 26);
+        int rs = (instructionWord >>> 21);
+        int rt = (instructionWord >>> 16);
+        int imm = instructionWord & 0xFFFF;
         int[] operands = {rs, rt, imm};
 
         String mnemonic = InstructionRegistry.getByOpcode(opcode);
         executeInstruction(configuration, mnemonic, operands);
     }
 
-    private static void simulateRType(Configuration configuration, int bin) {
-        int opcode = (bin >> 26);
-        int rs = (bin >>> 21);
-        int rt = (bin >>> 16);
-        int rd = (bin >>> 11);
-        int sa = (bin >>> 6);
-        int fun = bin & 0x3F;
+    private static void simulateRType(Configuration configuration, int instructionWord) {
+        int opcode = (instructionWord >> 26);
+        int rs = (instructionWord >>> 21);
+        int rt = (instructionWord >>> 16);
+        int rd = (instructionWord >>> 11);
+        int sa = (instructionWord >>> 6);
+        int fun = instructionWord & 0x3F;
         int[] operands = {rs, rt, rd, sa, fun};
 
         String mnemonic = InstructionRegistry.getByOpcode(opcode);
         executeInstruction(configuration, mnemonic, operands);
     }
 
-    private static void simulateJType(Configuration configuration, int bin) {
-        int opcode = (bin >> 26);
-        int iindex = bin & 0x03FFFFFF;
+    private static void simulateJType(Configuration configuration, int instructionWord) {
+        int opcode = (instructionWord >> 26);
+        int iindex = instructionWord & 0x03FFFFFF;
         int[] operands = {iindex};
 
         String mnemonic = InstructionRegistry.getByOpcode(opcode);

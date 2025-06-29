b0d483e9a660e3965cb07112cae2bbf6d80a202f
Konstantine Machavariani <kotiko2004@gmail.com>
2025-06-29 17:06:42 +0400

    Fix tests
    Signed-off-by: Konstantine Machavariani <kotiko2004@gmail.com>

--- src/test/java/com/wars/Instruction/JTypeInstructionTest.java
+++ src/test/java/com/wars/Instruction/JTypeInstructionTest.java
@@ -1,6 +1,5 @@
 package com.wars.Instruction;
 
-import com.wars.exception.AssemblerException;
 import com.wars.instruction.Instruction;
 import com.wars.instruction.InstructionRegistry;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,44 +9,28 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JTypeInstructionTest {
 
     @ParameterizedTest
     @MethodSource("provideValidJTypeInstructions")
-    void testJTypeInstructions(String mnemonic, String[] operands, String expectedBinary) {
-//        Instruction instruction = InstructionRegistry.create(mnemonic, operands);
-//        assertEquals(expectedBinary.replaceAll("_", ""), instruction.toBinaryString());
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideInvalidJTypeInstructions")
-    void testInvalidJTypeInstructions(String mnemonic, String[] operands) {
-//        assertThrows(AssemblerException.class, () -> InstructionRegistry.create(mnemonic, operands));
+    void testJTypeInstructions(String mnemonic, int[] operands, String expectedBinary) {
+        Instruction instruction = InstructionRegistry.create(mnemonic, operands);
+        assertEquals(expectedBinary.replaceAll("_", ""), instruction.toBinaryString());
     }
 
     private static Stream<Arguments> provideValidJTypeInstructions() {
         return Stream.of(
                 // j
-                Arguments.of("j", new String[]{"1048575"}, "000010_00000011111111111111111111"),
-                Arguments.of("j", new String[]{"0"}, "000010_00000000000000000000000000"),
-                Arguments.of("j", new String[]{"524287"}, "000010_00000001111111111111111111"),
-                Arguments.of("j", new String[]{"67108863"}, "000010_11111111111111111111111111"),
+                Arguments.of("j", new int[]{1048575}, "000010_00000011111111111111111111"),
+                Arguments.of("j", new int[]{0}, "000010_00000000000000000000000000"),
+                Arguments.of("j", new int[]{524287}, "000010_00000001111111111111111111"),
+                Arguments.of("j", new int[]{67108863}, "000010_11111111111111111111111111"),
                 // jal
-                Arguments.of("jal", new String[]{"1048575"}, "000011_00000011111111111111111111"),
-                Arguments.of("jal", new String[]{"0"}, "000011_00000000000000000000000000"),
-                Arguments.of("jal", new String[]{"524287"}, "000011_00000001111111111111111111"),
-                Arguments.of("jal", new String[]{"67108863"}, "000011_11111111111111111111111111")
+                Arguments.of("jal", new int[]{1048575}, "000011_00000011111111111111111111"),
+                Arguments.of("jal", new int[]{0}, "000011_00000000000000000000000000"),
+                Arguments.of("jal", new int[]{524287}, "000011_00000001111111111111111111"),
+                Arguments.of("jal", new int[]{67108863}, "000011_11111111111111111111111111")
                 );
-    }
-
-    private static Stream<Arguments> provideInvalidJTypeInstructions() {
-        return Stream.of(
-                // Invalid iindex (greater than 67,108,863)
-                Arguments.of("j", new String[]{"67108865"}),
-                Arguments.of("j", new String[]{"67108866"}),
-                Arguments.of("j", new String[]{"671088660"})
-        );
     }
 }
